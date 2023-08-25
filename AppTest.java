2c36c27f1dbeb7f5e6547f47d4586c0d02337e20
Vinit Shetty <vkshetty@deakin.edu.au>
2023-08-25 12:29:53 +1000

    Add files via upload
    Initial Commit

---
+++ AppTest.java
@@ -0,0 +1,11 @@
+package com.myapp;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);  // A simple test assertion
+    }
+}
